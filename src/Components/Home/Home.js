b60d97d93dd12a6ef424dec935f29624ede7614f
Sahan Randika <shnrndk@gmail.com>
2020-11-12 12:46:25 +0530

    popover added and summarized news changed

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -12,18 +12,18 @@ function Home(props) {
 
     const [state, setstate] = useState({
         HotNews: [
-            { id: 1, title: 'Hello', description: ' leo ut imperdiet tincidunt. Proin vel dui tempor, gravida arcu ac, hendrerit neque. Phasellus non malesuada est. Sed id tristique enim, sed blandit nulla. ' },
-            { id: 2, title: 'Hello1', description: ' leo ut imperdiet tincidunt. Proin vel dui tempor, gravida arcu ac, hendrerit neque. Phasellus non malesuada est. Sed id tristique enim, sed blandit nulla. ' },
-            { id: 3, title: 'Hello2', description: ' leo ut imperdiet tincidunt. Proin vel dui tempor, gravida arcu ac, hendrerit neque. Phasellus non malesuada est. Sed id tristique enim, sed blandit nulla. ' },
-            { id: 4, title: 'Hello3', description: ' leo ut imperdiet tincidunt. Proin vel dui tempor, gravida arcu ac, hendrerit neque. Phasellus non malesuada est. Sed id tristique enim, sed blandit nulla. ' },
-            { id: 5, title: 'Hello4', description: ' leo ut imperdiet tincidunt. Proin vel dui tempor, gravida arcu ac, hendrerit neque. Phasellus non malesuada est. Sed id tristique enim, sed blandit nulla. ' }
+            { news_id: 1, title: 'Hello', summerized_article: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ac diam vel leo gravida tincidunt. Nulla placerat sapien eros, sit amet commodo nisl accumsan ac. Nunc enim velit, ullamcorper ultrices efficitur id, eleifend a erat. Sed varius scelerisque sem quis facilisis. Praesent ac ultricies lorem. Nam pellentesque metus accumsan risus sagittis euismod. Integer ac tincidunt urna. Aliquam metus velit, tempus id varius molestie, consequat sed ex. Suspendisse sit amet facilisis urna. Suspendisse eget nisi vel quam ultricies scelerisque sed a augue. Fusce tincidunt sapien at diam sagittis tincidunt. Proin vitae eros id velit viverra varius a eget mi. Nam rhoncus erat eget luctus sodales. In hac habitasse platea dictumst.Etiam sed eros blandit, volutpat magna eget, tincidunt urna. Donec mattis vel ipsum in tristique. Phasellus laoreet rhoncus commodo. Curabitur porta, lectus non ultrices maximus, diam augue semper diam, quis aliquam ante sapien ac felis. Phasellus accumsan libero nisi, ut pharetra lectus iaculis maximus. Nullam facilisis suscipit porttitor. Suspendisse euismod vel justo nec mollis. Praesent enim massa, varius gravida ex id, congue pretium est. Aliquam et porttitor diam. Vivamus dapibus arcu vitae iaculis imperdiet. Vestibulum egestas facilisis hendrerit. Cras eleifend, turpis vel laoreet euismod, lorem odio gravida justo, ac dapibus ipsum turpis eu diam. Mauris dignissim commodo dui, quis finibus est mollis a. Quisque ut placerat tellus, vitae lacinia turpis. Donec convallis odio quis neque dapibus, dapibus suscipit dolor dictum. leo ut imperdiet tincidunt. Proin vel dui tempor, gravida arcu ac, hendrerit neque. Phasellus non malesuada est. Sed id tristique enim, sed blandit nulla. ',body:''},
+            { news_id: 2, title: 'Hello', summerized_article: ' leo ut imperdiet tincidunt. Proin vel dui tempor, gravida arcu ac, hendrerit neque. Phasellus non malesuada est. Sed id tristique enim, sed blandit nulla. ',body:''},
+            { news_id: 3, title: 'Hello', summerized_article: ' leo ut imperdiet tincidunt. Proin vel dui tempor, gravida arcu ac, hendrerit neque. Phasellus non malesuada est. Sed id tristique enim, sed blandit nulla. ',body:''},
+            { news_id: 4, title: 'Hello', summerized_article: ' leo ut imperdiet tincidunt. Proin vel dui tempor, gravida arcu ac, hendrerit neque. Phasellus non malesuada est. Sed id tristique enim, sed blandit nulla. ',body:''},
+            { news_id: 5, title: 'Hello', summerized_article: ' leo ut imperdiet tincidunt. Proin vel dui tempor, gravida arcu ac, hendrerit neque. Phasellus non malesuada est. Sed id tristique enim, sed blandit nulla. ',body:''},
         ],
         RecommendedNews:[
-            { id: 1, title: 'Hello', description: ' leo ut imperdiet tincidunt. Proin vel dui tempor, gravida arcu ac, hendrerit neque. Phasellus non malesuada est. Sed id tristique enim, sed blandit nulla. ' },
-            { id: 2, title: 'Hello1', description: ' leo ut imperdiet tincidunt. Proin vel dui tempor, gravida arcu ac, hendrerit neque. Phasellus non malesuada est. Sed id tristique enim, sed blandit nulla. ' },
-            { id: 3, title: 'Hello2', description: ' leo ut imperdiet tincidunt. Proin vel dui tempor, gravida arcu ac, hendrerit neque. Phasellus non malesuada est. Sed id tristique enim, sed blandit nulla. ' },
-            { id: 4, title: 'Hello3', description: ' leo ut imperdiet tincidunt. Proin vel dui tempor, gravida arcu ac, hendrerit neque. Phasellus non malesuada est. Sed id tristique enim, sed blandit nulla. ' },
-            { id: 5, title: 'Hello4', description: ' leo ut imperdiet tincidunt. Proin vel dui tempor, gravida arcu ac, hendrerit neque. Phasellus non malesuada est. Sed id tristique enim, sed blandit nulla. ' }
+            { news_id: 1, title: 'Hello', summerized_article: ' leo ut imperdiet tincidunt. Proin vel dui tempor, gravida arcu ac, hendrerit neque. Phasellus non malesuada est. Sed id tristique enim, sed blandit nulla. ',body:''},
+            { news_id: 2, title: 'Hello', summerized_article: ' leo ut imperdiet tincidunt. Proin vel dui tempor, gravida arcu ac, hendrerit neque. Phasellus non malesuada est. Sed id tristique enim, sed blandit nulla. ',body:''},
+            { news_id: 3, title: 'Hello', summerized_article: ' leo ut imperdiet tincidunt. Proin vel dui tempor, gravida arcu ac, hendrerit neque. Phasellus non malesuada est. Sed id tristique enim, sed blandit nulla. ',body:''},
+            { news_id: 4, title: 'Hello', summerized_article: ' leo ut imperdiet tincidunt. Proin vel dui tempor, gravida arcu ac, hendrerit neque. Phasellus non malesuada est. Sed id tristique enim, sed blandit nulla. ',body:''},
+            { news_id: 5, title: 'Hello', summerized_article: ' leo ut imperdiet tincidunt. Proin vel dui tempor, gravida arcu ac, hendrerit neque. Phasellus non malesuada est. Sed id tristique enim, sed blandit nulla. ',body:''},
         ]
     })
 
@@ -56,16 +56,30 @@ function Home(props) {
                 <Title style={{ margin: '20px 0px 20px 30px' }} level={5}>News Title</Title>
                 <Carousel responsive={responsive} autoPlay={true}>
                     {
-                        state.HotNews.map(i => {
-                            return <MainNewsCard key={i.id} newsData={i} />
+                        state.HotNews && state.HotNews.map(i => {
+                            const maxLength=120
+                            let summary;
+                            if(i.summerized_article.length >= maxLength) {
+                                summary = i.summerized_article.substr(0,maxLength).concat('...')
+                            }else{
+                                summary = i.summerized_article
+                            }
+                            return <MainNewsCard key={i.id} summary={summary} newsData={i} />
                         })
                     }
                 </Carousel>
                 <Title style={{ margin: '20px 0px 20px 30px' }} level={5}>News Title</Title>
                 <Carousel responsive={responsive} autoPlay={true} >
                 {
-                        state.RecommendedNews.map(i => {
-                            return <MainNewsCard key={i.id} newsData={i}/>
+                        state.RecommendedNews && state.HotNews.map(i => {
+                            const maxLength=120
+                            let summary;
+                            if(i.summerized_article.length >= maxLength) {
+                                summary = i.summerized_article.substr(0,maxLength).concat('...')
+                            }else{
+                                summary = i.summerized_article
+                            }
+                            return <MainNewsCard key={i.id} summary={summary} newsData={i} />
                         })
                     }
                 </Carousel>
--- src/Components/Layout/MainNewsCard.js
+++ src/Components/Layout/MainNewsCard.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Card, Rate, Button } from 'antd';
+import { Card, Rate, Button, Popover } from 'antd';
 import flag from '../../assets/flag.png'
 
 const { Meta } = Card;
@@ -7,29 +7,38 @@ const { Meta } = Card;
 function MainNewsCard(props) {
     console.log(props.newsData)
     return (
-            <Card
-              hoverable={true}
-                style={{ width: 300,borderColor:"red",
-                filter: 'drop-shadow(2px 4px 4px rgba(174, 0, 0, 0.25))',height:'370px'}}
-                cover={
-                  <img
+        <Card
+            hoverable={true}
+            style={{
+                width: 300, borderColor: "red",
+                filter: 'drop-shadow(2px 4px 4px rgba(174, 0, 0, 0.25))', height: '370px'
+            }}
+            cover={
+                <img
                     alt="example"
                     src={flag}
-                    style={{objectFit:'cover',height:'150px'}}
-                  />
-                }
-                actions={[
-                  <Rate value={3} />,
-                  <Button danger type="text" >
-                  වැඩි විස්තර 
-                </Button>,
-                ]}
-              >
-                <Meta
-                  title={props.newsData.title}
-                  description={props.newsData.description}
+                    style={{ objectFit: 'cover', height: '150px' }}
                 />
-              </Card>
+            }
+            actions={[
+                <Rate value={3} />,
+
+                <Button danger type="text" >
+                    වැඩි විස්තර
+                </Button>,
+
+            ]}
+        >
+
+
+
+            <Popover content={props.newsData.summerized_article} style={{maxWidth:'50%'}}>
+                <Meta
+                    title={props.newsData.title}
+                    description={props.summary}
+                />
+            </Popover>
+        </Card >
     )
 }
 

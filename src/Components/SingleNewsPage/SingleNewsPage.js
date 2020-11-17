c4003535f8181788eaa6494539aba09777a7e3be
Sahan Randika <shnrndk@gmail.com>
2020-11-17 17:19:33 +0530

    spinners added for single new page

--- src/Components/SingleNewsPage/SingleNewsPage.js
+++ src/Components/SingleNewsPage/SingleNewsPage.js
@@ -1,21 +1,44 @@
 import React, { useState, useEffect } from 'react'
 import Navbar from '../Layout/Navbar'
-import { Layout, Typography } from 'antd';
+import { Layout, Typography, Card, Divider, Spin } from 'antd';
 const { Footer, Content } = Layout;
-const { Title } = Typography;
+const { Title, Text } = Typography;
 
 
 function SingleNewsPage(props) {
 
-    useEffect(()=>{
+    const [state, setstate] = useState({
+        title: '',
+        body: ''
+    })
+
+    useEffect(() => {
         console.log(props.location.state)
-    },[props.location.state])
+        props.location.state && setstate({
+            ...props.location.state
+        })
+    }, [props.location.state])
     return (
         <Layout style={{ minHeight: "100vh" }}>
             <Navbar />
-            <Content style={{ backgroundColor: 'white' }}>
-                <Title style={{ margin: '20px 0px 20px 30px' }} level={5}>Hello</Title>
-                
+            <Content style={{ backgroundColor: 'white', display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
+                {
+                    (state.title == '') ?
+                        <div style={{ width: '1000px', display: 'flex', alignItems: 'center', justifyContent: 'center', height: '70vh' }}>
+                            <Spin size="large" />
+                        </div>
+
+
+                        :
+                        <React.Fragment>
+                            <Title style={{ margin: '20px 0px 20px 30px', marginTop: "30px", textAlign: 'center' }} level={3}>{state.title}</Title>
+                            <Card style={{ width: '80%' }} bordered={false}>
+                                <Text>{state.body}</Text>
+                            </Card>
+                        </React.Fragment>
+
+
+                }
 
             </Content>
             <Footer style={{ backgroundColor: 'white' }}>Footer</Footer>

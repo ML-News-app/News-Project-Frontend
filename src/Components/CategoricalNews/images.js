b5f3197c7d3ed7f41e6eddfc1ef73a0ac3391791
Sahan Randika <shnrndk@gmail.com>
2020-12-08 21:07:12 +0530

    images added

--- src/Components/CategoricalNews/images.js
+++ src/Components/CategoricalNews/images.js
@@ -5,7 +5,10 @@ let CricketNational = [
 
 let CricketInternational = [
     'https://www.shethepeople.tv/wp-content/uploads/2020/03/Womens-Cricket-Final-e1597648753577.jpg',
-    'https://www.sportekz.com/wp-content/uploads/2019/12/Cricket-Series-Schedule-2020-21.jpg'
+    'https://www.sportekz.com/wp-content/uploads/2019/12/Cricket-Series-Schedule-2020-21.jpg',
+    'https://ichef.bbci.co.uk/onesport/cps/624/cpsprodpb/11AF4/production/_115863427_jonny_bairstow_getty2.jpg',
+    'https://cdn.britannica.com/s:500x350/63/211663-050-A674D74C/Jonny-Bairstow-batting-semifinal-match-England-Australia-2019.jpg',
+    `https://img.cricketworld.com/images/e-085234/ndia's-shreyas-iyer-celebrates.jpg`
 ]
 
 let Football = [
@@ -13,11 +16,15 @@ let Football = [
     'https://d3vlf99qeg6bpx.cloudfront.net/content/uploads/2020/11/28143307/gross-1.jpg',
     'https://c.ndtvimg.com/2020-04/hhauvif8_tajikistan-football-afp_625x300_26_April_20.jpg',
     'https://img.fifa.com/image/upload/t_s3/huiggmkn0de5oaqh3gm8.jpg',
-    'https://www2.pictures.zimbio.com/gi/Alexandre+Lacazette+Jeremy+Chopilin+FC+Metz+faZyS8skypzl.jpg'
+    'https://www2.pictures.zimbio.com/gi/Alexandre+Lacazette+Jeremy+Chopilin+FC+Metz+faZyS8skypzl.jpg',
+    'https://cdn.britannica.com/53/74853-004-4C4F7CA7/Ronaldo-Brazil-players-match-German-2002-World-2002.jpg'
 ]
 
 let Rugby = [
-
+    'https://static01.nyt.com/images/2019/09/19/multimedia/19sp-shorties-inyt-1/merlin_140085294_f9329d76-08c4-43c1-8030-9b76f786acdc-jumbo.jpg',
+    'https://www.talkingrugbyunion.co.uk/images/e-024078/ml20200202129.jpg',
+    'https://www.norvanreports.com/wp-content/uploads/2020/09/rugby.jpg',
+    'https://www.americasrugbynews.com/wp-content/uploads/2020/02/jaguares-lucas-paulos-hurricanes-2020.jpg',
 ]
 
 let political = [

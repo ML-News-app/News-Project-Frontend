073277ebb04c348ea987a3045ffa33a595a6de72
Sahan Randika <shnrndk@gmail.com>
2020-12-12 18:39:56 +0530

    killswitch added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect,useState } from 'react'
 import {
   BrowserRouter as Router,
   Switch,
@@ -8,18 +8,38 @@ import Home from './Components/Home/Home';
 import './App.css';
 import CategoricalNews from './Components/CategoricalNews/CategoricalNews';
 import SingleNewsPage from './Components/SingleNewsPage/SingleNewsPage';
-
+import axios from 'axios';
 
 function App(props) {
+
+  const [state, setstate] = useState('')
+
+  useEffect(() => {
+    axios.get('https://mighty-springs-26141.herokuapp.com/killswitch/v1/pingme')
+      .then(response => {
+        setstate(response.data)
+      })
+  }, [])
+
   return (
 
-    <Router>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/category/:type" component={CategoricalNews} />
-        <Route exact path="/news" component={SingleNewsPage} />
-      </Switch>
-    </Router>
+
+    <React.Fragment>
+      {
+        state === 'harrypotter' ?
+          (
+            <Router>
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/category/:type" component={CategoricalNews} />
+                <Route exact path="/news" component={SingleNewsPage} />
+              </Switch>
+            </Router>
+          )
+          :
+          null
+      }
+    </React.Fragment>
 
   )
 }
